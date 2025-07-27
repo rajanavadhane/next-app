bebc4097cc06d3b036a23045e09034294a4d8e2a
rajanavadhane <rajanavadhane2000@gmail.com>
2025-07-27 15:17:37 +0530

    Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from "next"
-import "./globals.css"
-import { GeistMono } from "geist/font/mono"
 import { GeistSans } from "geist/font/sans"
+import { GeistMono } from "geist/font/mono"
+import type { Metadata } from "next"
+
 import Header from "./components/Header"
+import "./globals.css"
+import { BgGradient } from "./components/BgGradient"
+import { Footer } from "./components/Footer"
 
 export const metadata: Metadata = {
   title: "rajanavadhane.dev",
@@ -24,9 +27,17 @@ export default function RootLayout({
       <body className="flex min-h-screen flex-col font-sans md:max-w-7xl lg:mx-auto lg:flex-row">
         <main>
           <Header />
-        </main>
 
-        {children}
+          <div className="grid flex-1 grid-cols-1 lg:grid-cols-[32px_1fr_32px]">
+            <div className="hidden w-full border-r border-border-primary opacity-75 [background-image:linear-gradient(45deg,theme(colors.border-primary)_12.50%,transparent_12.50%,transparent_50%,theme(colors.border-primary)_50%,theme(colors.border-primary)_62.50%,transparent_62.50%,transparent_100%)] [background-size:5px_5px] lg:block"></div>
+            <div className="relative col-span-1 px-3 lg:px-0">
+              <BgGradient />
+              {children}
+            </div>
+            <div className="hidden w-full border-l border-border-primary opacity-75 [background-image:linear-gradient(45deg,theme(colors.border-primary)_12.50%,transparent_12.50%,transparent_50%,theme(colors.border-primary)_50%,theme(colors.border-primary)_62.50%,transparent_62.50%,transparent_100%)] [background-size:5px_5px] lg:block"></div>
+          </div>
+          <Footer />
+        </main>
       </body>
     </html>
   )
